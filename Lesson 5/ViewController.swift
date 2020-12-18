0aefae0d45cb2cc6460b0452d047123978d90ed0
shekarno <kirill_45@yahoo.com>
2020-12-19 04:00:28 +0500

    Initial Commit

---
+++ Lesson 5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 5
+//
+//  Created by Guest User on 19.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
