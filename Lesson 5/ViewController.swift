68db82559e90a41b1fb211b7094626bd4f748647
shekarno <kirill_45@yahoo.com>
2020-12-19 04:21:27 +0500

    0.1 lesson 5

--- Lesson 5/ViewController.swift
+++ Lesson 5/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "indexCell", for: <#T##IndexPath#>)
+    }
+    
+    
 }
 
